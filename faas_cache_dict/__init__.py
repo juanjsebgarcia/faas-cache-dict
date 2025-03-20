9256d444b60b663495e60fea43c93ff750ef1815
juanjsebgarcia <github@juangarcia.co.uk>
2025-03-20 18:11:49 +0000

    cut: delete the alpha file-backed faas cache dict implementation
    Signed-off-by: juanjsebgarcia <github@juangarcia.co.uk>

--- faas_cache_dict/__init__.py
+++ faas_cache_dict/__init__.py
@@ -1,6 +1,5 @@
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 
 from faas_cache_dict.exceptions import DataTooLarge  # noqa
 from faas_cache_dict.faas_cache_dict import FaaSCacheDict  # noqa
-from faas_cache_dict.file_faas_cache_dict import FileBackedFaaSCache  # noqa
